c2b51be5368a2a172645cdad0f9b59d26e894d9d
DIVYANSH2395 <divyanshusuwalka6@gmail.com>
2026-05-25 13:35:05 +0530

    Use MaterialApp with routes and initialRoute
    Replace the ad-hoc MyWidget runner with a MaterialApp configured for navigation. Sets debugShowCheckedModeBanner to false, defines initialRoute "/" -> Loading(), and adds named routes for "/home" -> Home() and "/location" -> Location(). Removes the unused MyWidget class to simplify app startup and enable named-route navigation.

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,24 @@ import 'package:mausam/Activity/Location.dart';
 
 void main() {
   runApp(
-    MyWidget()
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/",
+      routes: {
+        "/": (context) => Loading(),
+        "/home": (context) => Home(),
+        "/location": (context) => Location(),
+      },
+    ),
   );
-
-
-
-
 }
+    
+   
+  
 
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Loading(),
-    );
-  }
-}
+
+
+
+
